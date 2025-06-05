ead9afd0fc45839fb485dfa035ae59e6a6e7e2b5
Kunal Sinha <kunalsinh927@gmail.com>
2025-06-05 17:12:24 -0500

    build complete

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/atomiclyrics/',        // add this line
   plugins: [react()],
+  build: { outDir: 'dist' },
   test: {
-    globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.js'
   }
-});
+})
+
